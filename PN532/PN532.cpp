69ef4d274664b63b597c42a79205b3b21be726db
DC_RedStone <1516235445@qq.com>
2022-09-20 22:50:31 +0800

    Update PN532.cpp
    Fixed warning: warning: "SERIAL" redefined.

--- PN532/PN532.cpp
+++ PN532/PN532.cpp
@@ -42,13 +42,13 @@ void PN532::PrintHex(const uint8_t *data, const uint32_t numBytes)
 #ifdef ARDUINO
     for (uint8_t i = 0; i < numBytes; i++) {
         if (data[i] < 0x10) {
-            SERIAL.print(" 0");
+            theSERIAL.print(" 0");
         } else {
-            SERIAL.print(' ');
+            theSERIAL.print(' ');
         }
-        SERIAL.print(data[i], HEX);
+        theSERIAL.print(data[i], HEX);
     }
-    SERIAL.println("");
+    theSERIAL.println("");
 #else
     for (uint8_t i = 0; i < numBytes; i++) {
         printf(" %2X", data[i]);
@@ -73,22 +73,22 @@ void PN532::PrintHexChar(const uint8_t *data, const uint32_t numBytes)
 #ifdef ARDUINO
     for (uint8_t i = 0; i < numBytes; i++) {
         if (data[i] < 0x10) {
-            SERIAL.print(" 0");
+            theSERIAL.print(" 0");
         } else {
-            SERIAL.print(' ');
+            theSERIAL.print(' ');
         }
-        SERIAL.print(data[i], HEX);
+        theSERIAL.print(data[i], HEX);
     }
-    SERIAL.print("    ");
+    theSERIAL.print("    ");
     for (uint8_t i = 0; i < numBytes; i++) {
         char c = data[i];
         if (c <= 0x1f || c > 0x7f) {
-            SERIAL.print('.');
+            theSERIAL.print('.');
         } else {
-            SERIAL.print(c);
+            theSERIAL.print(c);
         }
     }
-    SERIAL.println("");
+    theSERIAL.println("");
 #else
     for (uint8_t i = 0; i < numBytes; i++) {
         printf(" %2X", data[i]);
